cf437a8f76cdc88b9a806487ecfd2d3754f7d8d9
Animesh Singh <blues1998@github.com>
2020-07-14 03:49:05 +0530

    Initial commit

---
+++ about.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+def main():
+    st.markdown("<h3 style='color:blue;font-weight:bold;'><u>About</h3>", unsafe_allow_html=True)
+    st.subheader('CODE FOR A BRIGHTER TOMORROW')
+
+    st.write('IBM Hack Challenge 2020 is all about coding for a cause. It’s about finding solutions to problems '
+             'plaguing society today. It is an opportunity to showcase your coding talent, learn new technologies and '
+             'build an efficiently working solution.')
+
+    st.markdown('<br>', unsafe_allow_html=True)
+    st.markdown("<h3 style='color:red;font-weight:bold;'><u>Our problem statement</h3>", unsafe_allow_html=True)
+    st.subheader('Sentiment Analysis of COVID-19 Tweets Visualization Dashboard')
+
+    st.write('''The sentiment analysis of Indians after the extension of lockdown announcements to be analyzed with the relevant #tags on twitter and build a predictive analytics model to understand the behavior of people if the lockdown is further extended.
+Also develop a dashboard with visualization of people reaction to the govt announcements on lockdown extension ''')
+
+
+if __name__ == '__main__':
+    main()
